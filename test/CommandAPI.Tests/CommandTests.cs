21532ceab53b563e961f907c8bdc5bf4f7dce56d
Enin Artem <artem.enin@onlyoffice.com>
2022-07-04 14:54:49 +0400

    full command object tests

--- test/CommandAPI.Tests/CommandTests.cs
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -2,23 +2,56 @@
 
 namespace CommandAPI.Tests;
 
-public class CommandTests
+public class CommandTests : IDisposable
 {
-    [Fact]
-    public void CanChangeHowTo()
+    private const string _howTo = "changed how to";
+    private const string _platform = "changed platform";
+    private const string _commandLine = "changed commandLine";
+
+    Command _testCommand;
+
+    public CommandTests()
     {
-        //Arrange
-        var testCommand = new Command
+        _testCommand = new Command
         {
             HowTo = "Do something awesome",
             Platform = "xUnit",
             CommandLine = "dotnet test"
         };
+    }
 
+    [Fact]
+    public void CanChangeHowTo()
+    {
         //Act
-        testCommand.HowTo = "Execute Unit Tests";
+        _testCommand.HowTo = _howTo;
 
         //Assert
-        Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        Assert.Equal(_howTo, _testCommand.HowTo);
+    }
+
+    [Fact]
+    public void CanChangePlatform()
+    {
+        //Act
+        _testCommand.Platform = _platform;
+
+        //Assert
+        Assert.Equal(_platform, _testCommand.Platform);
+    }
+
+    [Fact]
+    public void CanChangeCommandLine()
+    {
+        //Act
+        _testCommand.CommandLine = _commandLine;
+
+        //Assert
+        Assert.Equal(_commandLine, _testCommand.CommandLine);
+    }
+
+    public void Dispose()
+    {
+        _testCommand = null;
     }
 }
